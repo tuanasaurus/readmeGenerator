d56a1dc912c3de5b6ba0cf03672ff914f8b67318
Tuan Phan <tuan@tuan.design>
2020-09-16 22:43:00 -0700

    Added the axios API call to the GitHub API, added the table on content

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const axios = require('axios');
 
 //Array of question objects with three parameters and key value pairs.
 const questions = [
+    // {
+    //     type: 'input',
+    //     name: 'badge',
+    //     message: {
+    //         "schemaVersion": 1,
+    //         "label": "Student Project",
+    //         "message": "FREE TO USE",
+    //         "color": "orange"
+    //     }
+    // },
     {
         type: 'input',
         name: 'title',
@@ -27,11 +37,6 @@ const questions = [
         type: 'input',
         name: 'description',
         message: 'Please write a brief description about your project.',
-    },
-    {
-        type: 'input',
-        name: 'tableOfContents',
-        message: 'What are the content section of your readme, please separate each section with a comma?',
     },
     {
         type: 'input',
@@ -61,15 +66,29 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'profileImage',
-        message: 'Who are the contributor(s) to this project?',
+        name: 'test',
+        message: 'How do you test the app?',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'What is an email that a user can use to contact you?',
     },
 ]
 
 //Writing the function to fs to append the file
-
-inquirer.prompt(questions).then((response) => {
+inquirer.prompt(questions).then(async (response) => {
+    let userAvatar = '';
     console.log(response);
+    console.log(response.gitUsername);
+    // Calling the Github API for the user's info
+    const queryUrl = `https://api.github.com/users/${response.gitUsername}`;
+    try {
+        const { data } = await axios.get(queryUrl);
+        userAvatar = data.avatar_url;
+    } catch (e) {
+        console.log(e);
+    }
 
     const error = function (err) {
         if (err) {
@@ -77,24 +96,27 @@ inquirer.prompt(questions).then((response) => {
         } else {
             console.log("Success!");
         }};
-    //Title of the project with string interpolation to have cleaner line of codes
+    //fs.appendFileSync string interpolation to have cleaner line of codes
+    // fs.appendFileSync('ReadMe.md', `# ${response.badge}\n`, error);
     fs.appendFileSync('ReadMe.md', `# ${response.tile}\n`, error);
-    //Name of Repo = Name of the readme
     fs.appendFileSync('ReadMe.md', `# The Name of the repo:\n${response.repoName}\n`, error);
-    //Asking for the username then creating a section for it
-    fs.appendFileSync('ReadMe.md', `## Author:\n${response.gitUserName}\n`, error);
-    //Description section
+    fs.appendFileSync('ReadMe.md', `## Author:\n${response.gitUsername}\n`, error);
     fs.appendFileSync('ReadMe.md', `## Overview of the project\n${response.description}\n`, error);
-    //Table of content section with list and linking to the rest of the section
-    fs.appendFileSync('ReadMe.md', `## Table of Contents\n-${response.tableOfContents}\n`, error);
-    //Installation notes
+    // Made a table of content with anchor tag from the markdown
+    fs.appendFileSync('ReadMe.md', `## Table of Contents\n-[Installation](#Installation)
+        \n-[Usage](#Usage)
+        \n-[License](#License)
+        \n-[Contributing Member](#Contributing Member)
+        \n-[Test](#Test)
+        \n-[Reach Out](#Reach out)
+        \n`
+        , error);
     fs.appendFileSync('ReadMe.md', `## Installation\\n${response.installation}\n`, error);
-    //The usage of the app
     fs.appendFileSync('ReadMe.md', `## Usage\n${response.usage}\n`, error);
-    //The license of
     fs.appendFileSync('ReadMe.md', `## License\n${response.license}\n`, error);
-    //Name of Repo = Name of the readme
-    fs.appendFileSync('ReadMe.md', `# Contributing Member(s)\n${response.contributing}\n`, error);
+    fs.appendFileSync('ReadMe.md', `## Contributing Member\n${response.contributing}\n`, error);
+    fs.appendFileSync('ReadMe.md', `## Test\n${response.test}\n`, error);
+    fs.appendFileSync('ReadMe.md', `## Reach out\n${response.questions}\n![Me](${userAvatar})`, error);
 });
 
 // init();
